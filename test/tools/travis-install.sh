64b84c14fc28aaab407f5f497f1314d91ad8cf49
Stephane Alnet <stephane@shimaore.net>
2014-01-13 00:57:05 +0100

    travis: proper names and syntax, broke dpkg?

--- test/tools/travis-install.sh
+++ test/tools/travis-install.sh
@@ -14,5 +14,6 @@ gpg --armor --export "=Stephane Alnet (Packaging) <stephane@shimaore.net>" | apt
 apt-get update -qq
 
 ## Install FreeSwitch
-apt-get install --no-install-recommends -o 'Dpkg::Options::="--force-overwrite"' \
+echo 'Dpkg { Options {"--force-overwrite";} }' | tee -a /etc/apt/apt.conf
+apt-get install --no-install-recommends \
   freeswitch freeswitch-mod-commands freeswitch-mod-event-socket freeswitch-mod-dptools freeswitch-mod-loopback freeswitch-mod-dialplan-xml freeswitch-mod-sofia
