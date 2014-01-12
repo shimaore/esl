75a7fed65ac15f738e2d989ebf1a87499c3e338f
Stephane Alnet <stephane@shimaore.net>
2014-01-13 00:32:12 +0100

    travis: trying force-overwrite

--- test/tools/travis-install.sh
+++ test/tools/travis-install.sh
@@ -14,5 +14,5 @@ gpg --armor --export "=Stephane Alnet (Packaging) <stephane@shimaore.net>" | apt
 apt-get update -qq
 
 ## Install FreeSwitch
-apt-get install --no-install-recommends \
+apt-get install --no-install-recommends -o 'DPkg::options={"--force-overwrite";}' \
   freeswitch freeswitch-mod-commands freeswitch-mod-event-socket freeswitch-mod-dptools freeswitch-mod-loopback freeswitch-mod-dialplan-xml freeswitch-mod-sofia
