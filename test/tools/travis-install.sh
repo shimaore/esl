5b26dfd9eff483027cfe8eb1f5e480b7fab39eb0
Stephane Alnet <stephane@shimaore.net>
2014-01-12 22:41:13 +0100

    travis: tracking install issue

--- test/tools/travis-install.sh
+++ test/tools/travis-install.sh
@@ -5,4 +5,5 @@ gpg --recv-keys "F24B9200" && true
 gpg --recv-keys "F24B9200"
 gpg --armor --export "=Stephane Alnet (Packaging) <stephane@shimaore.net>" | apt-key add -
 apt-get update -qq
-apt-get install -qq freeswitch freeswitch-mod-commands freeswitch-mod-event-socket  freeswitch-mod-dptools freeswitch-mod-loopback freeswitch-mod-dialplan-xml freeswitch-mod-sofia
+apt-get install \
+  freeswitch freeswitch-mod-commands freeswitch-mod-event-socket freeswitch-mod-dptools freeswitch-mod-loopback freeswitch-mod-dialplan-xml freeswitch-mod-sofia
