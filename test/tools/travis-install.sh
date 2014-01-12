d74144d3aef00ae62f9379b4b51b12b560ddb92f
Stephane Alnet <stephane@shimaore.net>
2014-01-13 00:08:25 +0100

    travis: not using apt-add-repository, support is not consistent on travis.

--- test/tools/travis-install.sh
+++ test/tools/travis-install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 ## Add a repository with a recent FreeSwitch version.
-apt-get install -qq python-software-properties
-apt-add-repository "deb http://ftp.debian.org/debian testing main"
-apt-add-repository "deb http://debian.sotelips.net/shimaore shimaore main"
-# echo "deb http://debian.sotelips.net/shimaore shimaore main" | tee -a /etc/apt/sources.list
+echo "deb http://ftp.debian.org/debian testing main" | tee -a /etc/apt/sources.list
+echo "deb http://debian.sotelips.net/shimaore shimaore main" | tee -a /etc/apt/sources.list
 
+
+gpg --recv-keys "8B48AD6246925553" && true
+gpg --recv-keys "8B48AD6246925553"
+gpg --armor --export "=Debian Archive Automatic Signing Key (7.0/wheezy) <ftpmaster@debian.org>" | apt-key add -
 gpg --recv-keys "F24B9200" && true
 gpg --recv-keys "F24B9200"
 gpg --armor --export "=Stephane Alnet (Packaging) <stephane@shimaore.net>" | apt-key add -
