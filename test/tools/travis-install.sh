187990b524a8ade8f6c18064b0d4c82d24f2adde
Stephane Alnet <stephane@shimaore.net>
2014-01-12 23:26:06 +0100

    travis: apparently apt-add-repository isn't available

--- test/tools/travis-install.sh
+++ test/tools/travis-install.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
+
+## Upgrade Ubuntu to a version that would have our dependencies.
 apt-get install update-manager-core
 do-release-upgrade -d -m server -f DistUpgradeViewNonInteractive
 apt-get update
-apt-get install -qq python-software-properties
-apt-add-repository "deb http://debian.sotelips.net/shimaore shimaore main"
+
+## Add a repository with a recent FreeSwitch version.
+# apt-get install -qq python-software-properties
+# apt-add-repository "deb http://debian.sotelips.net/shimaore shimaore main"
+echo "deb http://debian.sotelips.net/shimaore shimaore main" | tee -a /etc/apt/sources.list
+
 gpg --recv-keys "F24B9200" && true
 gpg --recv-keys "F24B9200"
 gpg --armor --export "=Stephane Alnet (Packaging) <stephane@shimaore.net>" | apt-key add -
 apt-get update -qq
+
+## Install FreeSwitch
 apt-get install \
   freeswitch freeswitch-mod-commands freeswitch-mod-event-socket freeswitch-mod-dptools freeswitch-mod-loopback freeswitch-mod-dialplan-xml freeswitch-mod-sofia
