cf427e6c0351cc139a7a687a49504087b8682ff5
Stephane Alnet <stephane@shimaore.net>
2014-09-25 16:13:13 +0200

    Updated (working) initial tests with docker.io.

--- test/0001.sh
+++ test/0001.sh
@@ -2,22 +2,43 @@
 # set -e
 IMG=esl-test-0001
 for t in client server; do
+  echo "****** Building $t *******"
   docker build -t $IMG-$t 0001-$t/
 done
 
+DNS=$(dig +short docker-dns.local.localhost.docker-local @172.17.42.1 | egrep '^[0-9.]+$')
+echo
+echo "****** docker-dns is at $DNS *******"
+echo
+
+echo "****** Starting server *******"
 docker run -p 127.0.0.1:8022:8022 \
-  --dns=172.17.42.1 --dns-search=docker.local \
+  --dns=$DNS \
   -d --name $IMG-server $IMG-server
+echo "****** Starting client *******"
 docker run -p 127.0.0.1:8024:8024 \
-  --dns=172.17.42.1 --dns-search=docker.local \
-  --link esl-test-0001-server:server.local \
+  --dns=$DNS \
   -d --name $IMG-client $IMG-client
+
+echo "****** Ready *******"
 
 # Give FreeSwitch some time to settle
 # Give docker-dns some time to figure out there are new hosts (pollinterval = 17).
 sleep 20
-../node_modules/.bin/mocha --compilers coffee.md:coffee-script .
+../node_modules/.bin/mocha -t 10000 --compilers coffee.md:coffee-script/register -R spec .
+
+echo "---------------------------------------------------------------------------"
+dig esl-test-0001-server.local.localhost.docker-local @172.17.42.1
+
+# Postmortem
 for t in client server; do
+  echo "---------------------------------------------------------------------------"
+  echo "---------------------------------------------------------------------------"
+  echo "---------------------------------------------------------------------------"
+  echo "----------------- $t --------------------------------------------------"
+  echo "---------------------------------------------------------------------------"
+  echo "---------------------------------------------------------------------------"
+  echo "---------------------------------------------------------------------------"
   docker logs $IMG-$t;
   docker kill $IMG-$t;
   docker rm $IMG-$t;
--- test/README.md
+++ test/README.md
@@ -1,10 +1,5 @@
 Prerequisites
 =============
 
-To test the client you need:
-
-    aptitude install freeswitch freeswitch-mod-commands freeswitch-mod-event-socket
-
-To test the server you also need:
-
-    aptitude install freeswitch-mod-dptools freeswitch-mod-loopback
+- /etc/init.d/docker start
+- ./docker-dns.sh
